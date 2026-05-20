bac37650f69c7cf7c1f102f1db273e0f0b4257cd
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2026-05-20 22:34:23 +0200

    add logging for replay posting

--- tm-server-bridge/src/state.rs
+++ tm-server-bridge/src/state.rs
@@ -67,17 +67,20 @@ pub async fn setup_state_synchronization() {
         while seconds > 0 {
             match std::fs::read(&full_path) {
                 Ok(file) => {
-                    SPACETIME
-                        .read()
-                        .procedures
-                        .post_round_replay(event.time, file);
+                    SPACETIME.read().procedures.post_round_replay_then(
+                        event.time,
+                        file,
+                        |_, res| {
+                            tracing::info!("Posting replay returned: {res:?}");
+                        },
+                    );
                     if let Err(error) = std::fs::remove_file(&full_path) {
                         tracing::error!("Failed to delete the current replay file! Reason: {error}")
                     };
                     return;
                 }
                 Err(error) => {
-                    tracing::error!("Failed to read replay file. Reason: {error}")
+                    tracing::error!("Failed to read replay file {full_path}. Reason: {error}")
                 }
             };
 
